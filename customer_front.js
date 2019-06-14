26d652e7493fc4b02bdfab8d82664bf5e1195252
KYpotato <s0methingfun313@gmail.com>
2019-06-14 17:01:20 +0900

    put files together

--- customer_front.js
+++ customer_front.js
@@ -5,22 +5,73 @@ window.send = send;
 
 async function verify() {
   try {
+    // let result = await timelock.vefiry_address(
+    //   document.getElementById('address').value,
+    //   document.getElementById('redeem_script_1').value,
+    //   document.getElementById('pubkey_customer').value
+    // )
+    let address_and_redeem = document.getElementById('address_and_redeem').value;
+    let address;
+    let redeem;
+    let amount;
+
+    var tmp_str = address_and_redeem.split('?');
+    if(tmp_str[0].indexOf('bitcoin:') == 0) {
+      address = tmp_str[0].substr('bitcoin:'.length);
+    }
+    else{
+      throw new Error('invalid address and redeem');
+    }
+
+    var options = tmp_str[1].split('&');
+    if(options[2].indexOf('message=') == 0){
+      redeem = options[2].substr('message='.length);
+    }
+    else{
+      throw new Error('invalid address and redeem');
+    }
+    if(options[0].indexOf('amount=') == 0){
+      amount = options[0].substr('amount='.length);
+    }
+    else{
+      throw new Error('invalid address and redeem');
+    }
     let result = await timelock.vefiry_address(
-      document.getElementById('address').value,
-      document.getElementById('redeem_script').value,
+      address,
+      redeem,
       document.getElementById('pubkey_customer').value
     )
 
     if(result.result) {
-      document.getElementById('result').innerText = 'success';
+      document.getElementById('verified_result').innerText = 'success';
+      document.getElementById('div_deposit_address').style.visibility = "visible";
+      document.getElementById('div_deposit_amount').style.visibility = "visible";
+      document.getElementById('deposit_address').innerText = address;
+      document.getElementById('deposit_amount').innerText = amount;
+      document.getElementById('address_qr').src = 
+        "https://chart.googleapis.com/chart?cht=qr&chs=200x200&chco=000000&chl=bitcoin:" + address +
+        "?amount=" + amount;
+      document.getElementById('address_qr').style.visibility = "visible";
     }
     else {
-      document.getElementById('result').innerText = result.message;
+      document.getElementById('div_deposit_address').style.visibility = "hidden";
+      document.getElementById('div_deposit_amount').style.visibility = "hidden";
+      document.getElementById('deposit_address').innerText = '';
+      document.getElementById('deposit_amount').innerText = '';
+      document.getElementById('address_qr').src = '';
+      document.getElementById('address_qr').style.visibility = "hidden";
+      document.getElementById('verified_result').innerText = result.message;
       alert(result.message);
     }
 
   } catch (error) {
-    document.getElementById('result').innerText = 'error';
+    document.getElementById('div_deposit_address').style.visibility = "hidden";
+    document.getElementById('div_deposit_amount').style.visibility = "hidden";
+    document.getElementById('deposit_address').innerText = '';
+    document.getElementById('deposit_amount').innerText = '';
+    document.getElementById('address_qr').src = '';
+    document.getElementById('address_qr').style.visibility = "hidden";
+    document.getElementById('verified_result').innerText = 'error';
     alert(error);
   }
 }
@@ -29,7 +80,7 @@ async function send() {
 
   try {
     let result = await timelock.broadcast_tx_by_costomer(
-      document.getElementById('redeem_script').value,
+      document.getElementById('redeem_script_2').value,
       document.getElementById('secret').value,
       document.getElementById('target_address').value,
       document.getElementById('privkey_customer').value
--- restaurant_front.js
+++ restaurant_front.js
@@ -7,20 +7,27 @@ function gen_address() {
   try {
     let {redeemScript, address} = timelock.gen_timelock_address(
       document.getElementById('pubkey_restaurant').value,
-      document.getElementById('locktime').value,
+      document.getElementById('locktime_1').value,
       document.getElementById('secret').value,
       document.getElementById('pubkey_customer').value,
     );
 
-    document.getElementById('address').innerText = address;
-    document.getElementById('redeem_script').innerText = redeemScript.toString('hex');
-          
-    document.getElementById('address_qr').src = "https://chart.googleapis.com/chart?cht=qr&chs=200x200&chco=000000&chl=bitcoin:" + address;
+    let amount = parseFloat(document.getElementById('deposit_amount').value);
+
+    document.getElementById('generated_address').innerText = address;
+    document.getElementById('generated_redeem_script').innerText = 
+      "bitcoin:" + address + "?amount=" + amount + "&label=reservation" + "&message=" + redeemScript.toString('hex');
+    
+    document.getElementById('address_qr').src = 
+      "https://chart.googleapis.com/chart?cht=qr&chs=200x200&chco=000000&chl=bitcoin:" + address +
+      "?amount=" + amount +
+      "%26label=reservation" +
+      "%26message=" + redeemScript.toString('hex');
     document.getElementById('address_qr').style.visibility = "visible";
   } catch(error) {
 
-    document.getElementById('address').innerText = '';
-    document.getElementById('redeem_script').innerText = '';
+    document.getElementById('generated_address').innerText = '';
+    document.getElementById('generated_redeem_script').innerText = '';
     document.getElementById('address_qr').src = '';
     document.getElementById('address_qr').style.visibility = "hidden";
     alert(error);
@@ -31,7 +38,7 @@ async function send(){
   try{
     let result = await timelock.broadcast_tx_by_restaurant(
       document.getElementById('redeem_script').value,
-      document.getElementById('locktime').value,
+      document.getElementById('locktime_2').value,
       document.getElementById('target_address').value,
       document.getElementById('privkey_restaurant').value,
     );
